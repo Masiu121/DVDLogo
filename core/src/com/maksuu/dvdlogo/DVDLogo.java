d753eef116dd102772db6cb77a5f04d261ec92b4
Masiu121 <maks.kapolka@gmail.com>
2021-12-17 09:26:24 +0100

    Added colors

--- core/src/com/maksuu/dvdlogo/DVDLogo.java
+++ core/src/com/maksuu/dvdlogo/DVDLogo.java
@@ -8,36 +8,55 @@ import com.badlogic.gdx.utils.ScreenUtils;
 
 public class DVDLogo extends ApplicationAdapter {
 	SpriteBatch batch;
-	Texture img;
+	Texture pink;
+	Texture green;
+	Texture red;
+	Texture violet;
+	Texture mainTexture;
 	int x, y, xSpeed, ySpeed;
 	int height, width;
 
 	@Override
 	public void create () {
 		batch = new SpriteBatch();
-		img = new Texture("DVD_logo.png");
+		pink = new Texture("DVD_logo_pink.png");
+		green = new Texture("DVD_logo_green.png");
+		red = new Texture("DVD_logo_red.png");
+		violet = new Texture("DVD_logo_violet.png");
 		height = Gdx.graphics.getBackBufferHeight();
 		width = Gdx.graphics.getBackBufferWidth();
 		xSpeed = -3;
 		ySpeed = 3;
 		x = 0;
-		y = height - img.getHeight();
+		y = height - pink.getHeight();
+
+		mainTexture = pink;
 	}
 
 	@Override
 	public void render () {
 		update();
-		ScreenUtils.clear(1, 0, 0, 1);
+		ScreenUtils.clear(0, 0, 0, 1);
 		batch.begin();
-		batch.draw(img, x, y);
+		batch.draw(mainTexture, x, y);
 		batch.end();
 	}
 
 	public void update() {
-		if(x <= 0 || x >= width - img.getWidth()) {
+		if(x <= 0) {
+			mainTexture = pink;
 			xSpeed = xSpeed * -1;
 		}
-		if(y <= 0 || y >= height - img.getHeight()) {
+		if(x >= width - pink.getWidth()) {
+			mainTexture = green;
+			xSpeed = xSpeed * -1;
+		}
+		if(y <= 0) {
+			mainTexture = violet;
+			ySpeed = ySpeed * -1;
+		}
+		if(y >= height - pink.getHeight()) {
+			mainTexture = red;
 			ySpeed = ySpeed * -1;
 		}
 		x += xSpeed;
@@ -47,6 +66,9 @@ public class DVDLogo extends ApplicationAdapter {
 	@Override
 	public void dispose () {
 		batch.dispose();
-		img.dispose();
+		red.dispose();
+		pink.dispose();
+		violet.dispose();
+		green.dispose();
 	}
 }
